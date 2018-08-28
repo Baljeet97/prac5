d327fd25a7bc0281eeda25866f3604031e0618b3
jc448262 <baljeetsingh@my.jcu.edu.au>
2018-08-28 13:48:38 +1000

    prac 5

---
+++ color_hex.py
@@ -0,0 +1,11 @@
+COLOR_NAMES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
+                "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
+                "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
+                "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
+                "aquamarine4": "#458b74", "azure1": "#f0ffff",
+                "azure2": "#e0eeee", "azure3": "#c1cdcd", "azure4": "#838b8b",
+                "beige": "#f5f5dc", "bisque1": "#ffe4c4"}
+color_name = input("Enter color name: ")
+while color_name != "":
+    print("The color code for \"{}\" is \"{}\"".format(color_name, COLOR_NAMES.get(color_name)))
+    color_name = input("Enter color name: ")
